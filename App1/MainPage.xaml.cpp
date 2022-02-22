ea64d8a668f44832cd157ba384c9e05c3538f0f5
Remy PALLABOT <r_pallabot@hotmail.fr>
2022-02-22 10:15:26 +0100

    first real push

---
+++ App1/MainPage.xaml.cpp
@@ -0,0 +1,81 @@
+﻿//
+// MainPage.xaml.cpp
+// Implémentation de la classe MainPage.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include <thread>
+#include <shared_mutex>
+
+using namespace App1;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+using namespace std;
+
+shared_mutex VerInc, VerDec;
+INT Cpt;
+
+static UINT Inc() {
+	Sleep(1);
+	while (true) {
+		VerInc.lock();
+		Cpt++;
+	}
+	return 0;
+}
+
+static UINT Dec() {
+	Sleep(1);
+	while (true) {
+		VerDec.lock();
+		Cpt--;
+	}
+	return 0;
+}
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+	Cpt = rand();
+
+	DispatcherTimer^ timer = ref new DispatcherTimer; 
+	TimeSpan ts;
+	ts.Duration = 500000;
+	timer->Start();
+	timer->Tick += ref new EventHandler<Object^>(this, &MainPage::OnTick);
+
+	thread Th_Inc(Inc);
+	Th_Inc.detach();
+
+	thread Th_Dec(Dec);
+	Th_Dec.detach();
+}
+
+
+void App1::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	VerInc.unlock();
+}
+
+void App1::MainPage::Button_Click_1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	VerDec.unlock();
+}
+
+void App1::MainPage::OnTick(Platform::Object^ sender, Platform::Object^ e)
+{
+	Affich->Text = Cpt.ToString();
+}
---
+++ App1/MainPage.xaml.h
@@ -0,0 +1,25 @@
+﻿//
+// MainPage.xaml.h
+// Déclaration de la classe MainPage.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace App1
+{
+	/// <summary>
+	/// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Button_Click_1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void OnTick(Platform::Object^ sender, Platform::Object^ e);
+	};
+}
